bfb2d8ff0af42e7afa4bc8753788a7ab3ecaeac6
Maxim <isaevmaxim71@gmail.com>
2015-10-21 09:58:03 +0300

    Revert "Test_challenge"

--- src/main/java/com/news_item/News.java
+++ src/main/java/com/news_item/News.java
@@ -2,43 +2,35 @@ package com.news_item;
 
 public class News {
 
-    private static String guid;
+
     private static String title;
     private static String description;
     private static String author;
     private static String pubdate;
-    private static String link;
     private static int id;
 
-    public static void setGuid(String guid) {
-        News.guid = guid;
-    }
 
-    public static void setTitle(String title) {
+    public void setTitle(String title) {
         News.title = title;
     }
 
-    public static void setDescription(String description) {
+    public void setDescription(String description) {
         News.description = description;
     }
 
-    public static void setAuthor(String author) {
+    public void setAuthor(String author) {
         News.author = author;
     }
 
-    public static void setPubdatee(String pubdate) {
+    public void setPubdatee(String pubdate) {
         News.pubdate = pubdate;
     }
 
-    public static void setId(int id) {
+    public void setId(int id) {
         News.id = id;
     }
 
-    public static void setLink(String link) {
-        News.link = link;
-    }
-
-    public static String getTitle() {
+    public String getTitle() {
         return title;
     }
 
--- src/main/java/com/xml_parser/XMLParser.java
+++ src/main/java/com/xml_parser/XMLParser.java
@@ -1,65 +1,75 @@
 package com.xml_parser;
 
+import com.json_news_item.Fill_JSON;
 import com.json_news_item.JSONContainer;
+import com.news_item.News;
+import com.news_item.NewsContainer;
 import org.json.JSONObject;
+import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.*;
 
-import java.io.File;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
+public class XMLParser extends DefaultHandler {
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-
-public class XMLParser {
-
-
-    public static void parser() {
-        try {
-
-            DocumentBuilder xml = DocumentBuilderFactory.
-                    newInstance().newDocumentBuilder();
+    private NewsContainer items;
+    private News item;
+    private boolean inItem = false;
+    JSONObject newsJson;
 
 
-            Document doc = xml.parse(new File("D:/Tomcat/rss.builder.feedrss.builder.xml"));
+    private StringBuilder content;
 
-            Element rootel = doc.getDocumentElement();
-
-            System.out.println(rootel.getNodeName());
-
-            System.out.println("Child elements: ");
-            NodeList lst = rootel.getChildNodes();
-            NodeList itemList;
+    public XMLParser() {
+        content = new StringBuilder();
+    }
 
 
-            for (int i = 0; i < lst.getLength(); i++) {
-                System.out.println(lst.item(i).getNodeName());
-                NodeList channelNodes = lst.item(i).getChildNodes();
-                System.out.println("Channels child elements: " + channelNodes.getLength());
-                for (int j = 2; j < channelNodes.getLength(); j++) {
-                    System.out.println(channelNodes.item(j).getNodeName() + (j - 1));
-                    itemList = channelNodes.item(j).getChildNodes();
-                    displayItemChild(itemList);
-                }
+    @Override
+    public void startDocument() throws SAXException {
+
+    }
+
+    @Override
+    public void startElement(String namespaceURI, String localName, String qName, Attributes atts) throws SAXException {
+
+        content = new StringBuilder();
+        if (localName.equalsIgnoreCase("item")) {
+            inItem = true;
+            item = new News();
+            newsJson = new JSONObject();
+        }
+    }
+
+    @Override
+    public void endElement(String namespaceURI, String localName, String qName) throws SAXException {
+        if (localName.equalsIgnoreCase("title")) {
+            if (inItem) {
+                item.setTitle(content.toString());
             }
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        } else if (localName.equalsIgnoreCase("description")) {
+            if (inItem) {
+                item.setDescription(content.toString());
+            }
+        } else if (localName.equalsIgnoreCase("author")) {
+            if (inItem) {
+                item.setAuthor(content.toString());
+            }
+        } else if (localName.equalsIgnoreCase("pubdate")) {
+            if (inItem) {
+                item.setPubdatee(content.toString());
+            }
+        } else if (localName.equalsIgnoreCase("item")) {
+            inItem = false;
+            NewsContainer.setNewsList(item);
+            Fill_JSON.addJSON(newsJson, item);
         }
-
-
     }
 
-    public static void displayItemChild(NodeList list) {
-
-        JSONObject jsonObject = new JSONObject();
-        for (int i = 0; i < list.getLength(); i++) {
-            jsonObject.put(list.item(i).getNodeName(), list.item(i).getTextContent());
-            System.out.println(" " + list.item(i).getNodeName() + ":" + list.item(i).getTextContent());
-        }
-        JSONContainer.addNewJsonRecord(jsonObject);
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        content.append(ch, start, length);
     }
+
+    @Override
+    public void endDocument(){}
 }
-
 
